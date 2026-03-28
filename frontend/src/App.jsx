941bdbad32b2d4eba4e4545b1e833417bd185869
Ksoni-2311 <k2006soni@gmail.com>
2026-03-28 18:24:24 +0530

    Co-authored-by: Kjayesh0210 <Kjayesh0210@users.noreply.github.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import NavBar from './pages/NavBar.jsx'
 import LandingPage from './pages/LandingPage.jsx'
+import VendorDetails from './pages/VendorDetails.jsx'
+import CheckoutPage from './pages/CheckoutPage.jsx'
+import PlanPage from './pages/PlanPage.jsx'
 
 const App = () => {
   return (
     <div>
       {/* <NavBar /> */}
       <LandingPage />
+      {/* <PlanPage /> */}
+      <VendorDetails />
+      {/* <CheckoutPage /> */}
     </div>
   )
 }
