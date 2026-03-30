d078ed3138bce451d0875da7057a2cb6dfe54082
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-03-30 11:52:44 +0530

    food model

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
 import providerRoutes from './routes/provider.route.js';
+import foodRoutes from './routes/fooodRoutes.js';
 const app = express();
 
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/auth",authRoutes);
 app.use("/api/providers",providerRoutes);
+app.use("/api/foods", foodRoutes);
 
 
 app.get('/',(req,res) => {
